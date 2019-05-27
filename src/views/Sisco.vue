1f35cd91d723ed1fb235d1ef72f49112ce6d3fd2
newtonius <mateusfrancofs@hotmail.com>
2019-05-27 10:56:57 -0300

    graph componente nao vai ser nevessario

--- src/views/Sisco.vue
+++ src/views/Sisco.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <NavBar></NavBar>
     <div v-if="this.permission">
     </div>
 
     <div v-else>
       <div class="row">
-        <div class="col s12 m6 l2"><p>s12 m6 l2</p></div>
-        <div class="col s12 m6 l2"><p>s12 m6 l2</p></div>
-        <div class="col s12 m6 l2"><p>s12 m6 l2</p></div>
-        <div class="col s12 m6 l2"><p>s12 m6 l2</p></div>
-        <div class="col s12 m6 l2"><p>s12 m6 l2</p></div>
+        <div class="col s12 m6 l3"></div>
+        <div class="col s12 m6 l3"></div>
+        <div class="col s12 m6 l3"></div>
+        <div class="col s12 m6 l3"></div>
+      </div>
+      <div class="row">
+
       </div>
     </div>
   </div>
@@ -18,22 +19,21 @@
 
 <script>
 
-import Navbar from '@/components/NavBar.vue'
 
 export default {
   name: 'siscohome',
   components:{
-    Navbar
+  },
+  mounted() {
+    var div = document.getElementById('teste').innerHTML = '<h1>testando</h1>'
+
   },
   data() {
     return {
-      
       permission: false
     }
-  },
-  mounted() {
-    
-  },
+  }
+  
 }
 </script>
 
